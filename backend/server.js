759bb7ee474b58e44f50ab6845d5b468f903d23e
iamankityadav04-bit <iamankityadav04@gmail.com>
2026-05-22 21:14:58 +0530

    Fix proxy issue

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const port = process.env.PORT || 5000;
 
 app.use(helmet());
